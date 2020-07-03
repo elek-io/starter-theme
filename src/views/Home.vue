339dcac3cc62e1bb38f3a48e810568364dd32e97
Nils Kolvenbach <nils.kolvenbach@7p-group.com>
2020-07-03 13:21:04 +0200

    Changed layout style in config and added some blocks for testing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <elek.io:block id="welcome-message" only="paragraph, link">A default welcome message!</elek.io:block>
   </div>
 </template>
 
