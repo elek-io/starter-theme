331a622c6791e23f4888afc5a4144f72c83aa283
Nils Kolvenbach <nils.kolvenbach@7p-group.com>
2020-07-03 13:30:03 +0200

    Changed HTML element style

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <elek.io:block id="about-content">Start writing something about this page here</elek.io:block>
+    <elek-io-block id="about-content">Start writing something about this page here</elek-io-block>
     <hr>
-    <elek.io:block id="about-footer" not="heading"></elek.io:block>
+    <elek-io-block id="about-footer" not="heading"></elek-io-block>
   </div>
 </template>
