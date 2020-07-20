2daf8b88906449638d9e6242e1ab453bfda0110a
Nils Kolvenbach <nils.kolvenbach@7p-group.com>
2020-07-20 22:48:48 +0200

    Added post page to test taxonomies

---
+++ src/views/Post.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="post">
+    <ElekIo elek-io type="block" id="post-content" :blocks="blocks"></ElekIo>
+  </div>
+</template>
+
+<script>
+import ElekIo from '@/components/ElekIo.vue'
+
+export default {
+  name: 'Post',
+  components: {
+    ElekIo
+  },
+  props: {
+    blocks: Array
+  }
+}
+</script>
