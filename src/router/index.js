335cfc6a45215fb4b513171cddd94e8e9e55837b
Nils Kolvenbach <nils.kolvenbach@7p-group.com>
2020-08-12 21:57:25 +0200

    implemented working dynamic routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Project from '../../.elek.io/project.json'
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
+const routes = Project.pages.map((page) => {
+  return {
+    path: page.path,
+    name: page.name,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(`../../${page.layout.path}`)
   }
-]
+});
 
 const router = new VueRouter({
   mode: 'history',
